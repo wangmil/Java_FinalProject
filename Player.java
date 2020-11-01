1fc6bdc94ec3701708b2f467de14be59556c1952
wangmil <56725683+wangmil@users.noreply.github.com>
2020-11-01 14:22:12 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,33 +3,35 @@ import javax.swing.*;
 
 public class Player {
 	 int x,y,w,h;
-	 double jumpSpeed = 50,gravity = 3,acceleration = 1, jumpHeight = 100;
-	 boolean grounded = true, jump = false;
-	 ImageIcon catImg=null;
+	 double jumpSpeed = 120,gravity = 3,acceleration = 1, jumpHeight = 100;
+	 boolean grounded = true, jump = false, spaceship = false, die = false;;
+	 ImageIcon player=null;
 	 public Player() {
-		 catImg= new ImageIcon("geoDash.png");
+		 
 		 x=425;
 		 y=425;
 		 w=50;
 		 h=50;
 	 }
 	 public void myDraw(Graphics g) {
-		 g.drawImage(catImg.getImage(), x, y, w,h,null);
-		 g.drawRect(x, y, w,h);
+		 if(die == true) {
+			 player= new ImageIcon("bakaretsu.png");
+		 }else if(spaceship == true) {
+			 player= new ImageIcon("spaceship.png");
+		 } else {
+			 player= new ImageIcon("geoDash.png");
+		 }
+		 g.drawImage(player.getImage(), x, y, w,h,null);
 	 }
 	 public void jump() {
 		 if(grounded == true) {
 			 acceleration +=0.4;
 			 this.y-=jumpSpeed/acceleration;
 			 if(jumpSpeed/acceleration < 15) {
-				 acceleration = 1;
 				 grounded = false;
 				 jump = false;
 			 }
 		 }
-	 }
-	 public void die() {
-		 catImg= new ImageIcon("bakaretsu.png");
 	 }
 
 
