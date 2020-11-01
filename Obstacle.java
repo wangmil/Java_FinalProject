21928176f9c535670bf564ab1f26267e3dc6ce10
wangmil <56725683+wangmil@users.noreply.github.com>
2020-11-01 14:21:24 -0500

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -2,30 +2,44 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+import javax.swing.ImageIcon;
+
 
 
 public class Obstacle extends Rectangle{ //randomize this - have either spikes or normal blocks
-	int  width, height;
+	int  width, height, x,y,type;
+	double speed = 30;
+	boolean surfaceTrue = false;
 	Color c;
+	ImageIcon obstacle=null;
 	static int cnt=0;
 	int id=0;
-	public Obstacle() {
+	public Obstacle(int x, int y,int width, int height) {
 		id=cnt++;
-		x= 900;
-		y= 425;
-		width = 400;
-		height = 50;
-		c= new Color(0,0,0);
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
 	}
 	public void myDraw(Graphics g) {
-		g.setColor(c);
-		g.fillRect(x, y, width, height);
-		g.drawString("ID: " +id, x, y-25);
-		g.setColor(new Color(255,255,255));
-		g.drawRect(x, y, width ,height);
+		if(type == 0) {
+			obstacle= new ImageIcon("platform.png");
+			
+			
+		} else if(type == 1) {
+			obstacle= new ImageIcon("spike.png");
+			
+		} else if(type == 2) {
+			obstacle= new ImageIcon("spaceshiportal.png");
+			
+		} else if(type == 3) {
+			obstacle= new ImageIcon("portal.png");
+			
+		}
+		g.drawImage(obstacle.getImage(), x, y, width,height,null);
 	}
 	public void myMove() {
-		x-=30;
+		x-=speed;
 	}
 	public boolean getYCollision(int x, int y, int h, int w) {
 		if(y + h <= this.y && x +w >= this.x &&  x <= width + this.x) {
@@ -35,10 +49,10 @@ public class Obstacle extends Rectangle{ //randomize this - have either spikes o
 		}
 	}
 	public boolean getXCollision(int x, int y, int h, int w) {
-		if(x+w>=this.x && x<this.x+width && y +h > this.y) {
+		if(x+w>this.x && x<this.x+width && y +h > this.y && y < this.y+this.height) {
 			return true;
 		} else {
-			return false;
+			return false; 
 		}
 	}
 	
